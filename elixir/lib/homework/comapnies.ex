0ab0bafbbf2e5f053e5b68c0349eefe2b2bfc9b6
Fahad Tariq <Fahad.Tariq@microfocus.com>
2021-02-04 18:28:33 -0700

    Adding controller for company

---
+++ elixir/lib/homework/comapnies.ex
@@ -0,0 +1,143 @@
+defmodule Homework.Companies do
+  @moduledoc """
+  The Companies context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Homework.Repo
+
+  alias Homework.Companies.Company
+  alias Homework.Transactions.Transaction
+
+  @doc """
+  Returns the list of companies.
+
+  ## Examples
+
+      iex> list_companies([])
+      [%Company{}, ...]
+
+  """
+  def list_comapnies(_args) do
+    Repo.all(Company)
+  end
+
+  @doc """
+  Gets a single company.
+
+  Raises `Ecto.NoResultsError` if the Company does not exist.
+
+  ## Examples
+
+      iex> get_company!(123)
+      %Company{}
+
+      iex> get_company!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_company!(id), do: Repo.get!(Company, id)
+
+  @doc """
+  Creates a company.
+
+  ## Examples
+
+      iex> create_company(%{field: value})
+      {:ok, %Company{}}
+
+      iex> create_companyt(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_company(attrs \\ %{}) do
+    %Company{}
+    |> Company.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a company.
+
+  ## Examples
+
+      iex> update_company(company, %{field: new_value})
+      {:ok, %Company{}}
+
+      iex> update_company(company, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_company(%Company{} = company, attrs) do
+    company
+    |> Company.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a company.
+
+  ## Examples
+
+      iex> delete_company(company)
+      {:ok, %Company{}}
+
+      iex> delete_company(company)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_company(%Company{} = company) do
+    Repo.delete(company)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking company changes.
+
+  ## Examples
+
+      iex> change_company(company)
+      %Ecto.Changeset{data: %Company{}}
+
+  """
+  def change_company(%Company{} = company, attrs \\ %{}) do
+    Company.changeset(company, attrs)
+  end
+
+
+
+  def update_available_credit(id) do
+     query = from t in Transaction,
+                  where: t.company_id == ^id,
+                  select: sum(t.amount)
+
+        case Repo.all(query) do
+        [] ->
+            {:error, "invalid query result"}
+        [used_credit] ->
+            IO.puts used_credit
+                used_credit = if !used_credit do
+                    0
+                    else
+                    used_credit
+                end
+            company = get_company!(id)
+            IO.puts company.credit_line
+            IO.puts used_credit
+            available_credit = company.credit_line - used_credit
+            IO.puts available_credit
+            args = %{
+            credit_line: company.credit_line,
+            id: id,
+            description: company.description,
+            name: company.name,
+            available_credit: available_credit
+            }
+            case update_company(company, args) do
+            {:ok, company} ->
+                {:ok, company}
+            error ->
+                {:error, "could not update company"}
+            end
+        end
+    end
+end
